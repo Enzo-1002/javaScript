189663fdd98ef3be62e8442d05aa15df77eb6120
Enzo-1002 <enzo.borelli@estudante.ifmt.edu.br>
2024-09-13 09:04:14 -0400

    Aplica a pasta exercícios

---
+++ exercicios/Repetição.java
@@ -0,0 +1,11 @@
+
+public class Repetição {
+    public static void main(String[] args){
+       int num = -1;
+
+        While (num < 11){
+            JOptionPane.showinputdialog(+num);
+            num++;
+        }
+    }
+}
